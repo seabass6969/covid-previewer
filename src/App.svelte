7d400f6272f692d476e1d6ff600fdca380d2a6a2
BuildTools <cephas8080@outlook.com>
2022-01-26 21:45:48 -0100

    first commit baby

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+    async function getCovid() {
+        let response = await fetch("https://disease.sh/v3/covid-19/all");
+        let covidall = await response.json();
+        return covidall;
+    }
+    const promise = getCovid();
+</script>
+
+<main>
+<h1>
+the globe covid case be like:
+</h1>
+{#await promise}
+    <p>freaking wait pls⌛</p>
+{:then covidall}
+    <p>the confirmed covid🦠 case is: {covidall.cases}</p>
+    <p>today covid🦠 cases is: {covidall.todayCases}</p>
+    <p>death💀 total is: {covidall.deaths}</p>
+    <p>today death💀 cases is {covidall.todayDeaths}</p>
+{:catch error}
+    <p>error ❌ the error is {error}</p>
+{/await}
+<h3>Write by Cephas with ❤ and time⏳</h3>
+</main>
+
+<style>
+</style>
