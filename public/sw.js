5fbb554abd218eff47f8c4a108b3331b3885c916
BuildTools <cephas8080@outlook.com>
2022-01-30 10:20:19 -0100

    done

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,16 @@
-console.log("testing")
+console.log("service worker loaded")
+let deferredPrompt;
+
+if (window !== undefined){
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+}
+
